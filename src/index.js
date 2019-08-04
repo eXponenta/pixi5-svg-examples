42a9abdeaba8232fde428159ea2d7dfbccc90706
Konstantin <rondo.devil@gmail.com>
2019-08-04 17:55:06 +0300

    Refact

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-import { Application, GraphicsGeometry } from "pixi.js";
-import Svg from "./svg";
+import { Application, Container, Graphics, Text } from "pixi.js";
+import Svg from "pixi5-svg";
 import { Viewport } from "pixi-viewport";
 
-import * as PIXI from "pixi.js";
-window.PIXI = PIXI;
-
-//GraphicsGeometry.BATCHABLE_SIZE = 100000;
+const tests = {
+	"a-test" : "Test arc",
+	"shape-test" : "Shapes test\n(miter bug)",
+	"rotate-test": "Rotate and translate test!\n(bounds bug)",
+	"rs-test": "Short bezier (C) test",
+	"s-test" : "Bezier (s) test",
+	"t-test" : "Bezier (Q, T) test\n(selfcrossing bug)",
+	"bee" : "Complex test: bee",
+	"tiger" : "Complex test: tiger"
+}
 
 const app = new Application({
 	width: window.innerWidth,
@@ -20,54 +26,44 @@ app.stage = new Viewport()
 	.pinch()
 	.wheel();
 
+const data = Object.keys(tests).map((e) => ({key : e, url : e + ".svg.txt"}));
 app.loader.baseUrl = "./data";
 app.loader
-	//.add("svg", "map2.svg.txt")
-	.add("tiger", "m-test.svg.txt")
+	.add(data)
 	.load(() => {
 
-	//const map = app.loader.resources["svg"].data;
-	const tiger = app.loader.resources["tiger"].data;
-	//c.appendChild(container);
+		const res = app.loader.resources;
+		const pw = app.screen.width / 4;
+		const ph = 	app.screen.height / 2;
+		const offset = 40;
+		const stroke = new Graphics().lineStyle(2, 0x0)
+		const textContainer = new Container();
+		const svgContainer = new Container()
 
-	//const svgMapObject = new Svg(svgMap, { unpackTree: false});
-	const svgTigerObject = new Svg(tiger, { unpackTree: false, fillColor: "red"});
-	
-/*
-	console.log(svgMapObject);
-	let objs = [...svgMapObject.children];
-	let index = 0;
+		data.forEach((e, index)=>{
+			const text = res[e.key].data;
+			const svg = new Svg(text);
 
-	while (objs[++index]) {
-		let e = objs[index];
-		if (e.type !== "g") {
-			e.interactive = true;
-			e.buttonMode = true;
-			e.on("pointerover", onHoverUp, this);
-			e.on("pointerout", onHowerDown, this);
-		} else {
-			objs.push(...e.children);
-		}
+			const x = pw * (index % 4), y = 80 + (index / 4 | 0) * (ph);
+			
+			const bounds = svg.getBounds();
+
+			const title = new Text(tests[e.key]);
+			const scale = Math.min( (pw - offset * 2) / bounds.width, (ph - 80 - offset * 2) / bounds.height);
+			
+			title.position.set(x, y - 10);
+			title.anchor.set(0,1);
+			
+			svg.scale.set(scale);
+			svg.pivot.set( bounds.x + bounds.width * 0.5, bounds.y + bounds.height * 0.5 );
+			svg.position.set( x + pw * .5, y + (ph-80) * .5);
+			
+			svgContainer.addChild(svg);
+			textContainer.addChild(title);
+			stroke.drawRect(x , y , pw, (ph - 80));
+		});
+
+		app.stage.addChild(stroke, svgContainer, textContainer)
 	}
-	*/
-	app.stage.addChild(svgTigerObject)//, svgMapObject, );
-});
-
-let _last = undefined;
-function onHoverUp(event) {
-	
-	if (_last) {
-		_last.tint = 0xffffff;
-	}
-	setTimeout(() => {
-		event.target.tint = 0xff0000;
-		_last = event.target;
-	}, 0);
-}
-
-function onHowerDown(event) {
-	if (_last) _last.tint = 0xffffff;
-	_last = undefined;
-}
-
+);
 c.appendChild(app.view);
